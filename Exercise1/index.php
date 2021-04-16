7009184318a29f266c3bc791708b4f5eed183357
Abhiram V <61599526+Anon-Artist@users.noreply.github.com>
2021-04-16 19:11:41 +0000

    Add files via upload

---
+++ Exercise1/index.php
@@ -0,0 +1,44 @@
+ <?php
+		 $file=$_GET['file'];
+		 include $file;
+		 ?>
+<html>
+    <head>
+            <title>Vuln Labs</title>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Play">
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Orbitron">
+        </head>
+        <style>
+            body {
+                background: url(bg.jpg) no-repeat center center fixed;
+                background-position: fixed;
+            }
+
+            h1 {
+                margin-top: 50px;
+                font-size: 60px;
+                color: white;
+                font-family: orbitron;
+                margin-bottom: 460px;
+            }
+
+            input[type=button] {
+                margin-top: 20px;
+                font-size: 40px;
+                color: whitesmoke;
+                border-radius: 6px;
+                border: 5px solid  whitesmoke;
+                background: transparent;
+                font-family: Play;
+            }
+
+            input[type=button]:hover {
+                background: white;
+                color: black;
+            }
+        </style>
+        <body>
+                <h1>File Inclusion Exercise</h1>
+                <!-- file parameter -->
+            </body>
+    </html>
